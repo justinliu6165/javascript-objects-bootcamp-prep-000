30cd371bef5fc2be7a6e25b378d7e0dcf67b5e66
Justin Liu <justin_liu@hotmail.co.uk>
2018-05-15 20:43:10 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {
-  'Bob Dylan': "The times they are a changin",
-  'Beach House': "Space song",
+  "Bob Dylan": "The times they are a changin",
+  "Beach House": "Space song",
 }
 
 function updatePlaylist(playlist, artistName, songTitle){
