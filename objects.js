221745b0a0a9bfb3dd925f87d07efa04db40bd10
Justin Liu <justin_liu@hotmail.co.uk>
2018-05-15 20:37:53 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ var playlist = {
   beachHouse: "Space song",
 }
 
-function updatePlaylist(object, artistName, songTitle){
-  object[artistName] = songTitle;
-  
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle;
+  returnplaylist;
 }
